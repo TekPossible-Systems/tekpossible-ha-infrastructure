ae5ad18d2776d23d23a2fb39849b3067c4579f5e
grifkies <grifkies@protonmail.com>
2024-05-18 12:55:02 -0600

    Added the base template code for CDK TS

--- bin/cdk_project.ts
+++ bin/cdk_project.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkProjectStack } from '../lib/cdk_project-stack';
+import { Happy } from '../lib/happy';
 
 const app = new cdk.App();
 new CdkProjectStack(app, 'CdkProjectStack', {
@@ -10,7 +10,7 @@ new CdkProjectStack(app, 'CdkProjectStack', {
    * but a single synthesized template can be deployed anywhere. */
 
   /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
+   * and Region that are implies
   // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 
   /* Uncomment the next line if you know exactly what Account and Region you
