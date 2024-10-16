2c1b7a0613d07da6f28fefd36847f817a06a301c
Griffin Kiesecker <gkiesecker@wks01.tekp.dev>
2024-10-15 23:29:42 -0600

    Changed some formatting stuff and committed the results so that there is less config delta

--- ami_config_modifier.py
+++ ami_config_modifier.py
@@ -8,4 +8,4 @@ with open("./config/config.json", "r") as infrastructure_config:
     infrastructure_config_json = json.load(infrastructure_config)
     infrastructure_config_json["ami"][region] = ami_id
     with open("./config/config.json", "w") as infrastructure_config_write:
-            json.dump(infrastructure_config_json, infrastructure_config_write)
+            json.dump(infrastructure_config_json, infrastructure_config_write, indent=4)
