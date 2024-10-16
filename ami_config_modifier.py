bedf0e048ca16830f6434a9e2f0640097916c27f
Griffin Kiesecker <gkiesecker@wks01.tekp.dev>
2024-10-15 23:24:46 -0600

    Added the python code for chaning the ami out

---
+++ ami_config_modifier.py
@@ -0,0 +1,11 @@
+import json
+import sys
+
+region = str(sys.argv[1])
+ami_id = str(sys.argv[2])
+
+with open("./config/config.json", "r") as infrastructure_config: 
+    infrastructure_config_json = json.load(infrastructure_config)
+    infrastructure_config_json["ami"][region] = ami_id
+    with open("./config/config.json", "w") as infrastructure_config_write:
+            json.dump(infrastructure_config_json, infrastructure_config_write)
