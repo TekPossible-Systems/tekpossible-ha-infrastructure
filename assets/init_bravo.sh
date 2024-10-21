a26fc20aecafe832cd11fdbf4b9df3f59ff298be
Griffin Kiesecker <gkiesecker@wks01.tekp.dev>
2024-10-20 22:06:16 -0600

    Added the updated user data and added a todo for the software install

--- assets/init_bravo.sh
+++ assets/init_bravo.sh
@@ -11,19 +11,9 @@ do
 done
 
 sudo firewall-cmd --reload
-
-# Wazuh Agent Deploy
-sudo dnf install httpd mod_ssl -y
-sudo systemctl enable --now httpd
-sudo dnf install -y uuid
 sudo hostnamectl set-hostname $(uuid -v 4)-BravoServer
-curl -o wazuh-agent-4.7.4-1.x86_64.rpm https://packages.wazuh.com/4.x/yum/wazuh-agent-4.7.4-1.x86_64.rpm
-sudo WAZUH_MANAGER='REPLACE' rpm -ivh wazuh-agent-4.7.4-1.x86_64.rpm
-sudo systemctl daemon-reload
-sudo systemctl enable wazuh-agent
-sudo systemctl start wazuh-agent
 
-# AWS SSN Agent Deploy
+# AWS SSM Agent Deploy
 sudo dnf install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
 sudo systemctl enable --now amazon-ssm-agent
 
@@ -36,17 +26,9 @@ sudo ./install auto
 
 # Update and reboot the system
 sudo dnf update -y
-# sudo shutdown -r -h +1
 # Todo: figure out the right grace time
 
 sudo echo "Hostname: $(hostname -f)" > /var/www/html/index.html
 sudo systemctl restart httpd
 
-# STIG Stuff 
-dnf install -y ansible-core wget unzip
-cd /home/ec2-user
-wget https://dl.dod.cyber.mil/wp-content/uploads/stigs/zip/U_RHEL_9_V1R2_STIG_Ansible.zip
-unzip U_RHEL_9_V1R2_STIG_Ansible.zip
-unzip rhel9STIG-ansible.zip
-sudo ansible-galaxy collection install ansible.posix community.general
-sudo bash ./enforce.sh 
+# TODO: ADD SOFTWARE INSTALL HOOKS HERE
