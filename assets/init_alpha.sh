4db86e3ade0d62a2360f595edac57d7027455f1f
Griffin Kiesecker <gkiesecker@wks01.tekp.dev>
2024-10-21 21:59:58 -0600

    Trying a restart of amazon-ssm-agent since I definitely have the right policy

--- assets/init_alpha.sh
+++ assets/init_alpha.sh
@@ -16,6 +16,7 @@ sudo hostnamectl set-hostname $(uuid -v 4)-AlphaServer
 # AWS SSM Agent Deploy
 sudo dnf install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
 sudo systemctl enable --now amazon-ssm-agent
+sudo systemctl restart amazon-ssm-agent
 
 # CodeDeploy Stuff
 cd /home/ec2-user
