2aad5544737991461c7ede22d22f847ef7f4c1c5
Griffin Kiesecker <gkiesecker@wks01.tekp.dev>
2024-10-24 20:28:39 -0600

    Added rpm gpgkey import

--- assets/init_alpha.sh
+++ assets/init_alpha.sh
@@ -12,6 +12,7 @@ done
 
 sudo firewall-cmd --reload
 sudo hostnamectl set-hostname $(uuid -v 4)-AlphaServer
+sudo rpm --import /etc/pki/rpm-gpg/amazon-gpg-key
 
 # AWS SSM Agent Deploy
 sudo dnf install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
